8a335c22c79658e8976832a8d342d15c52951e3d
orzr <tjsdkfk77@naver.com>
2023-02-24 18:39:41 +0900

    [Feat] 순수 JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,12 +2,23 @@ package hello.hellospring;
 
 import hello.hellospring.reposiotory.MemberRepository;
 import hello.hellospring.reposiotory.MemoryMemberRepository;
+import hello.hellospring.reposiotory.jdbcMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+       // return new MemoryMemberRepository();
+        return new jdbcMemberRepository(dataSource);
     }
 }
